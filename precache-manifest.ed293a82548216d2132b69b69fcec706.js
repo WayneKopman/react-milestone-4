6e24d13de14d18e16dfa1c6e4e4cd11392c1e7b2
Wayne Kopman <56189380+WayneKopman@users.noreply.github.com>
2020-08-03 21:50:08 +0100

    Updates

---
+++ precache-manifest.ed293a82548216d2132b69b69fcec706.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1531a8891717c0e7d041c1d1d8b50413",
+    "url": "/react-milestone-4/index.html"
+  },
+  {
+    "revision": "f45a19f46f77eb8eae7e",
+    "url": "/react-milestone-4/static/css/main.cb563af9.chunk.css"
+  },
+  {
+    "revision": "c158a8d9084bc76f0461",
+    "url": "/react-milestone-4/static/js/2.0f53809d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-milestone-4/static/js/2.0f53809d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f45a19f46f77eb8eae7e",
+    "url": "/react-milestone-4/static/js/main.297fc4de.chunk.js"
+  },
+  {
+    "revision": "7cb2a9ffbe55a7f814a3",
+    "url": "/react-milestone-4/static/js/runtime-main.5d07c87f.js"
+  },
+  {
+    "revision": "5c4352e8ea65dc4589f2dca28660664f",
+    "url": "/react-milestone-4/static/media/cold.5c4352e8.jpg"
+  },
+  {
+    "revision": "d9dc78424bf3ea917a882e7f0e392def",
+    "url": "/react-milestone-4/static/media/warm.d9dc7842.jpg"
+  }
+]);
